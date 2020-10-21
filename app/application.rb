1ea23f68bb644caa0f8fcaedaa9d442ed5e70f6a
Paris Dolfman <parisdolfman@gmail.com>
2020-10-21 13:45:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
         resp.write "Your cart is empty"
         else
           @@cart.each do |item|
-            resp.write "#{item}/n"
+            resp.write "#{item}"
           end
         end
 
